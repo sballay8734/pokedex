9bc44f27f6d2af2c7afbe2ee6af41d0ec09f0976
sballay8734 <48398020+sballay8734@users.noreply.github.com>
2023-07-03 19:23:01 -0400

    Completed main functionality for comparison chart.

--- src/components/CompareTable/CompareTable.jsx
+++ src/components/CompareTable/CompareTable.jsx
@@ -2,22 +2,31 @@ import "./compare-table.css"
 
 function CompareTable({pokeOne, pokeTwo}) {
   console.log(pokeOne)
-  return <table className="compare-table">
-    <thead>
-      <tr>
-        <th>{pokeOne ? pokeOne.name : "null"}</th>
-        <th>Stat</th>
-        <th>{pokeTwo ? pokeTwo.name : "null"}</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>{pokeOne ? pokeOne.stats[0].base_stat : "null"}</td>
-        <td>HP</td>
-        <td>{pokeTwo ? pokeTwo.stats[0].base_stat : "null"}</td>
-      </tr>
-    </tbody>
-  </table>
+  const pokeOneStat = 156
+  const pokeTwoStat = 110
+
+  return (
+    <div className="table-wrapper">
+      {/* <div style={{width: `${testWidth}px`}} className="progress-bar"></div> */}
+      <div className="row">
+        <div className="stat poke-one-stat">
+          <div className="poke-one-stat-value">{pokeOneStat}</div>
+          <div
+            style={{ width: `${pokeOneStat / 2.3}px` }}
+            className="poke-one-bar"
+          ></div>
+        </div>
+        <div className="stat-name">HP</div>
+        <div className="stat poke-two-stat">
+          <div
+            style={{ width: `${pokeTwoStat / 2.3}px` }}
+            className="poke-two-bar"
+          ></div>
+          <div className="poke-two-stat-value">{pokeTwoStat}</div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default CompareTable
--- src/components/PokeInfo/PokeInfo.jsx
+++ src/components/PokeInfo/PokeInfo.jsx
@@ -12,7 +12,7 @@ function PokeInfo() {
       <div className="poke-one">
         <h1 className="poke-one-header">Pokemon One</h1>
         {pokeOne ? <div className="poke-one-info">
-          <h1 className="compare poke-one-name">{pokeOne ? pokeOne.name : ""}</h1>
+          <h1 className="compare poke-one-name">{pokeOne ? pokeOne.name[0].toUpperCase() + pokeOne.name.substring(1) : ""}</h1>
           {pokeOne ? <img src={pokeOne.sprites.front_default} alt="Pokemon One" /> : ""}
         <div onClick={() => setPokeOne(null)} className="remove-poke-one">
           Remove
@@ -25,7 +25,7 @@ function PokeInfo() {
       <div className="poke-two">
         <h1 className="poke-two-header">Pokemon Two</h1>
         {pokeTwo ? <div className="poke-two-info">
-          <h1 className="compare poke-two-name">{pokeTwo ? pokeTwo.name : ""}</h1>
+          <h1 className="compare poke-two-name">{pokeTwo ? pokeTwo.name[0].toUpperCase() + pokeTwo.name.substring(1) : ""}</h1>
           {pokeTwo ? <img src={pokeTwo.sprites.front_default} alt="Pokemon Two" /> : ""}
         <div onClick={() => setPokeTwo(null)} className="remove-poke-two">
           Remove
